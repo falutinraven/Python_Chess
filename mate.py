61100fb2aebed70d55725ded5e196e577d2e6154
Falutin Raven <falutinraven@gmail.com>
2023-06-20 18:28:34 -0500

    added logic to the mate module

--- mate.py
+++ mate.py
@@ -1,6 +1,31 @@
 import move_validation
 
 
+def generate_directional_moves(game_board, vertical, horizontal, *move_info):
+    """
+        vertical is the rate of change  (1 = up, 0 = no change, -1 = down)
+        horizontal is the rate of change (1 = right, 0 = no change, -1 = left)
+    """
+    old_rank = move_info[0]
+    old_file = move_info[2]
+    name = move_info[4]
+    is_white = move_info[5]
+
+    possible_moves = []
+
+    new_rank = old_rank + vertical
+    new_file = old_file + horizontal
+    while True:
+        move_info = [old_rank, new_rank, old_file, new_file, name, is_white]
+        if not move_validation.is_possible_move(game_board, *move_info):
+            break
+        possible_moves.append(move_info)
+        new_file += horizontal
+        new_rank += vertical
+
+    return possible_moves
+
+
 def generate_white_pawn_moves(game_board, *move_info):
     """
         pass input of piece info and returns a list
@@ -88,198 +113,127 @@ def generate_knight_moves(game_board, *move_info):
 
 
 def generate_bishop_moves(game_board, *move_info):
-    old_rank = move_info[0]
-    old_file = move_info[2]
-    name = move_info[4]
-    is_white = move_info[5]
-
-    # theoretical_moves = []
     possible_moves = []
 
-    # north-east
-    new_rank = old_rank+1
-    new_file = old_file+1
-    while True:
-        move_info = [old_rank, new_rank, old_file, new_file, name, is_white]
-        if move_validation.is_possible_move(game_board, *move_info):
-            possible_moves.append(move_info)
-            new_rank += 1
-            new_file += 1
-            continue
-        else:
-            break
-
-    # south-east
-    new_rank = old_rank-1
-    new_file = old_file+1
-    while True:
-        move_info = [old_rank, new_rank, old_file, new_file, name, is_white]
-        if move_validation.is_possible_move(game_board, *move_info):
-            possible_moves.append(move_info)
-            new_rank -= 1
-            new_file += 1
-            continue
-        else:
-            break
-
-    # north-west
-    new_rank = old_rank+1
-    new_file = old_file-1
-    while True:
-        move_info = [old_rank, new_rank, old_file, new_file, name, is_white]
-        if move_validation.is_possible_move(game_board, *move_info):
-            possible_moves.append(move_info)
-            new_rank += 1
-            new_file -= 1
-            continue
-        else:
-            break
-
-    # south-west
-    new_rank = old_rank-1
-    new_file = old_file-1
-    while True:
-        move_info = [old_rank, new_rank, old_file, new_file, name, is_white]
-        if move_validation.is_possible_move(game_board, *move_info):
-            possible_moves.append(move_info)
-            new_rank -= 1
-            new_file -= 1
-            continue
-        else:
-            break
+    north_east = generate_directional_moves(game_board, 1, 1, *move_info)
+    possible_moves.extend(north_east)
+    south_east = generate_directional_moves(game_board, -1, 1, *move_info)
+    possible_moves.extend(south_east)
+    north_west = generate_directional_moves(game_board, 1, -1, *move_info)
+    possible_moves.extend(north_west)
+    south_west = generate_directional_moves(game_board, -1, -1, *move_info)
+    possible_moves.extend(south_west)
 
     return possible_moves
 
 
 def generate_rook_moves(game_board, *move_info):
+    possible_moves = []
+
+    right = generate_directional_moves(game_board, 0, 1, *move_info)
+    possible_moves.extend(right)
+    left = generate_directional_moves(game_board, 0, -1, *move_info)
+    possible_moves.extend(left)
+    up = generate_directional_moves(game_board, 1, 0, *move_info)
+    possible_moves.extend(up)
+    down = generate_directional_moves(game_board, -1, 0, *move_info)
+    possible_moves.extend(down)
+
+    return possible_moves
+
+
+def generate_king_moves(game_board, *move_info):
     old_rank = move_info[0]
     old_file = move_info[2]
     name = move_info[4]
     is_white = move_info[5]
 
-    # theoretical_moves = []
+    # TODO: include castling
+    theoretical_moves = [(old_rank+1, old_file+1),
+                         (old_rank+1, old_file),
+                         (old_rank+1, old_file-1),
+                         (old_rank, old_file+1),
+                         (old_rank, old_file-1),
+                         (old_rank-1, old_file+1),
+                         (old_rank-1, old_file),
+                         (old_rank-1, old_file-1)]
     possible_moves = []
 
-    # right
-    new_rank = old_rank
-    new_file = old_file+1
-    while True:
+    for move in theoretical_moves:
+        new_rank = move[0]
+        new_file = move[1]
         move_info = [old_rank, new_rank, old_file, new_file, name, is_white]
         if move_validation.is_possible_move(game_board, *move_info):
             possible_moves.append(move_info)
-            new_file += 1
-            continue
-        else:
-            break
-
-    # left
-    new_rank = old_rank
-    new_file = old_file-1
-    while True:
-        move_info = [old_rank, new_rank, old_file, new_file, name, is_white]
-        if move_validation.is_possible_move(game_board, *move_info):
-            possible_moves.append(move_info)
-            new_file -= 1
-            continue
-        else:
-            break
-
-    # up
-    new_rank = old_rank+1
-    new_file = old_file
-    while True:
-        move_info = [old_rank, new_rank, old_file, new_file, name, is_white]
-        if move_validation.is_possible_move(game_board, *move_info):
-            possible_moves.append(move_info)
-            new_rank += 1
-            continue
-        else:
-            break
-
-    # down
-    new_rank = old_rank-1
-    new_file = old_file
-    while True:
-        move_info = [old_rank, new_rank, old_file, new_file, name, is_white]
-        if move_validation.is_possible_move(game_board, *move_info):
-            possible_moves.append(move_info)
-            new_rank -= 1
-            continue
-        else:
-            break
 
     return possible_moves
 
 
-def generate_queen_moves(game_board, *move_info):
-    # old_rank = move_info[0]
-    # old_file = move_info[2]
-    # name = move_info[4]
-    # is_white = move_info[5]
-
-    # theoretical_moves = []
-    # possible_moves = []
-
-    # for move in theoretical_moves:
-    #     new_rank = move[0]
-    #     new_file = move[1]
-    #     move_info = [old_rank, new_rank, old_file, new_file, name, is_white]
-    #     if move_validation.is_possible_move(game_board, *move_info):
-    #         possible_moves.append(move_info)
-
-    # return possible_moves
-    pass
-
-
-def generate_king_moves(game_board, *move_info):
-    # old_rank = move_info[0]
-    # old_file = move_info[2]
-    # name = move_info[4]
-    # is_white = move_info[5]
-
-    # theoretical_moves = []
-    # possible_moves = []
-
-    # for move in theoretical_moves:
-    #     new_rank = move[0]
-    #     new_file = move[1]
-    #     move_info = [old_rank, new_rank, old_file, new_file, name, is_white]
-    #     if move_validation.is_possible_move(game_board, *move_info):
-    #         possible_moves.append(move_info)
-
-    # return possible_moves
-
-# for move in actual_possible_moves[old_rank][old_file]:
-#     if move_attempt == move:
-#         return true/move_piece
-    pass
-
-
-def generate_moves():
+def generate_moves(pieces, whites_turn, game_board):
     """
         make and return a list of all moves that are theoretically possible (call is_possible_move() in move_validation module)
-        from whoevers turn it is (whites turn or black) 
+        from whoevers turn it is (whites turn or black)
         ex: each pawn move will have max of 4 possible moves (not including promotions) and the possible moves will be added
         to a list of all theoretical moves that will be iterated through to check if any prevent check in future function.
+
+        *move_info = [old_rank, new_rank, old_file, new_file, name, is_white]
     """
-    # TODO: use list.extend() instead of append() to take the lists from each of the piece move generators into this final list
-    # TODO: confirm piece exists at location of move info. now all the checks are wether the move is possible, but it will do that move even if
-    # there is no correct piece at the old_rank and old_file location
     all_possible_moves = []
-    all_possible_moves.extend(generate_white_pawn_moves())
-    all_possible_moves.extend(generate_black_pawn_moves())
-    all_possible_moves.extend(generate_knight_moves())
-    all_possible_moves.extend(generate_bishop_moves())
-    all_possible_moves.extend(generate_rook_moves())
-    all_possible_moves.extend(generate_queen_moves())
-    all_possible_moves.extend(generate_king_moves())
+
+    for piece in pieces:
+        move_info = [piece.rank, None, piece.file, None, piece.name, piece.is_white]
+        if not piece:
+            continue
+        if piece.is_white is not whites_turn:
+            continue
+        if piece.name == 'p':
+            if piece.is_white:
+                all_possible_moves.extend(generate_white_pawn_moves(game_board,
+                                                                    *move_info))
+            else:
+                all_possible_moves.extend(generate_black_pawn_moves(game_board,
+                                                                    *move_info))
+        if piece.name == 'n':
+            all_possible_moves.extend(generate_knight_moves(game_board,
+                                                            *move_info))
+        if piece.name == 'b':
+            all_possible_moves.extend(generate_bishop_moves(game_board,
+                                                            *move_info))
+        if piece.name == 'r':
+            all_possible_moves.extend(generate_rook_moves(game_board,
+                                                          *move_info))
+        if piece.name == 'q':
+            all_possible_moves.extend(generate_bishop_moves(game_board,
+                                                            *move_info))
+            all_possible_moves.extend(generate_rook_moves(game_board,
+                                                          *move_info))
+        if piece.name == 'k':
+            all_possible_moves.extend(generate_king_moves(game_board,
+                                                          *move_info))
+
     return all_possible_moves
 
 
-def mate():
+def mate(pieces, whites_turn, game_board):
     """
        iterate through list of all moves that can be attempted (from generate_moves()) and will attempt_move() each one
        if there is no attempt_move() that succeeds then either checkmate or stalemate (worry about that later)
+       TODO: make attempt_move not actually move piece if piece can be moved
 
     """
-    pass
+    all_possible_moves = generate_moves(pieces, whites_turn, game_board)
+    for move_info in all_possible_moves:
+        if move_validation.attempt_move(pieces, game_board, *move_info):
+            print("whites turn = ", whites_turn)
+            print("ranks, ",
+                  move_info[0]+1, " ",
+                  move_info[1]+1, " ",)
+
+            print("files, ",
+                  move_info[2]+1, " ",
+                  move_info[3]+1, " ",)
+
+            print(move_info[4], " ",
+                  move_info[5])
+            return False
+    return True
